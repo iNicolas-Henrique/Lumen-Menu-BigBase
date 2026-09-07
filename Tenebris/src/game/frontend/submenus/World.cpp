be64fcc998ffbf27cfd9d01735645c682779606c
Nicolas Henrique Costa Santos <nh157040@gmail.com>
2026-09-07 20:09:26 -0300

    ui(world): move Indian Attack beside zombie minigame

--- Tenebris/src/game/frontend/submenus/World.cpp
+++ Tenebris/src/game/frontend/submenus/World.cpp
@@ -98,7 +98,7 @@ namespace YimMenu::Submenus
 				ImGui::Text("%s", std::format("{}: {}/{}", Localization::IsPortuguese() ? "Veículos" : "Vehicles", GetVehiclePool()->m_Size - GetVehiclePool()->GetNumFreeSlots(), GetVehiclePool()->m_Size).data());
 			if (GetObjectPool())
 				ImGui::Text("%s", std::format("{}: {}/{}", Localization::IsPortuguese() ? "Objetos" : "Objects", GetObjectPool()->m_Size - GetObjectPool()->GetNumFreeSlots(), GetObjectPool()->m_Size).data());
-		}, "Uso dos pools", "Mostra quantos PEDs, veículos e objetos estão carregados nos pools do jogo.", 300.0f);
+		}, "Uso dos pools", "Mostra quantos PEDs, veículos e objetos estão carregados nos pools do jogo.", 300.0f));
 
 		auto killPeds = std::make_shared<Group>("Eliminar", 1);
 		killPeds->AddItem(std::make_shared<CommandItem>("killallpeds"_J));
@@ -113,6 +113,8 @@ namespace YimMenu::Submenus
 		bringOpts->AddItem(std::make_shared<CommandItem>("bringobjs"_J));
 		auto minigames = std::make_shared<Group>("Minijogos", 1);
 		minigames->AddItem(std::make_shared<BoolCommandItem>("undeadnightmare"_J));
+		minigames->AddItem(std::make_shared<BoolCommandItem>("indianattack"_J));
+		minigames->AddItem(std::make_shared<BoolCommandItem>("nearbyhostilepeds"_J));
 		minigames->AddItem(std::make_shared<ConditionalItem>("undeadnightmare"_J, std::make_shared<BoolCommandItem>("zombieslogging"_J)));
 		minigames->AddItem(std::make_shared<ConditionalItem>("undeadnightmare"_J, std::make_shared<BoolCommandItem>("hardmode"_J)));
 		auto misc = std::make_shared<Group>("Diversos");
