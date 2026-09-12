7f3e68fb95dd92bcb3edbaf56b4ed12337fd0ece
Nicolas Henrique Costa Santos <nh157040@gmail.com>
2026-09-12 12:45:46 -0300

    perf(ui): shrink FPS overlay work area [skip ci]

--- Tenebris/src/game/frontend/Overlay.cpp
+++ Tenebris/src/game/frontend/Overlay.cpp
@@ -16,21 +16,22 @@ namespace YimMenu
 {
 	void Overlay::Draw()
 	{
-		if (!Features::_OverlayEnabled.GetState() || !NativeInvoker::AreHandlersCached())
+		if (!Features::_OverlayEnabled.GetState() || !Features::_OverlayShowFPS.GetState() || !NativeInvoker::AreHandlersCached())
 			return;
 
-		ImGui::SetNextWindowSize(ImVec2(*Pointers.ScreenResX - 10.0f, *Pointers.ScreenResY - 10.0f), ImGuiCond_Always);
 		ImGui::SetNextWindowPos(ImVec2(10.0f, 10.0f), ImGuiCond_Always);
 		ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0.0f, 0.0f, 0.0f, 0.0f));
 		ImGui::PushFont(Menu::Font::g_OverlayFont);
 
-		ImGui::Begin("##overlay", nullptr, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoInputs);
+		ImGui::Begin("##overlay", nullptr,
+		    ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove |
+		        ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoScrollbar |
+		        ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoInputs);
 
-		if (Features::_OverlayShowFPS.GetState())
-			ImGui::Text("FPS: %d", (int)(1 / MISC::GET_SYSTEM_TIME_STEP()));
+		ImGui::Text("FPS: %d", static_cast<int>(1.0f / MISC::GET_SYSTEM_TIME_STEP()));
 
+		ImGui::End();
 		ImGui::PopFont();
 		ImGui::PopStyleColor();
-		ImGui::End();
 	}
 }
