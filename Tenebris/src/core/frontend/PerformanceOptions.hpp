16380e02ea577f164df9b5937a33b87cfc5686b0
Nicolas Henrique Costa Santos <nh157040@gmail.com>
2026-09-12 12:43:31 -0300

    perf(ui): add visual performance switches [skip ci]

---
+++ Tenebris/src/core/frontend/PerformanceOptions.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "core/commands/BoolCommand.hpp"
+
+namespace YimMenu::PerformanceOptions
+{
+	extern BoolCommand MenuAnimations;
+	extern BoolCommand MenuShadows;
+	extern BoolCommand EditorAnimations;
+	extern BoolCommand NotificationAnimations;
+	extern BoolCommand NotificationProgressBar;
+}
