33d55eb08ab5149e6e8ab9b5730b244a29d31227
Nicolas Henrique Costa Santos <nh157040@gmail.com>
2026-09-12 12:12:19 -0300

    fix(world): remove orphaned Player Must Die menu command

--- Tenebris/src/game/frontend/submenus/World.cpp
+++ Tenebris/src/game/frontend/submenus/World.cpp
@@ -99,7 +99,9 @@ namespace YimMenu::Submenus
 		bringOpts->AddItem(std::make_shared<CommandItem>("bringvehs"_J));
 		bringOpts->AddItem(std::make_shared<CommandItem>("bringobjs"_J));
 		auto minigames = std::make_shared<Group>("Minijogos", 1);
-		minigames->AddItem(std::make_shared<BoolCommandItem>("nearbyhostilepeds"_J));
+		minigames->AddItem(std::make_shared<BoolCommandItem>("undeadnightmare"_J));
+		minigames->AddItem(std::make_shared<ConditionalItem>("undeadnightmare"_J, std::make_shared<BoolCommandItem>("zombieslogging"_J)));
+		minigames->AddItem(std::make_shared<ConditionalItem>("undeadnightmare"_J, std::make_shared<BoolCommandItem>("hardmode"_J)));
 		auto misc = std::make_shared<Group>("Diversos");
 		misc->AddItem(std::make_shared<BoolCommandItem>("disableguardzones"_J));
 		auto eventOverride = std::make_shared<Group>("", 1);
